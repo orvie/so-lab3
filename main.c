68bafbef44a93a9ee1964a98ef811fea46d09b86
orvie <orvie.se2@gmail.com>
2018-04-08 23:52:28 -0500

    Creacion de funcionalidad exit y pwd

--- main.c
+++ main.c
@@ -13,12 +13,58 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+//Para getcwd
+#include <unistd.h>
+#include <signal.h>
+#include <sys/wait.h>
+
+#include "parser.h"
+
+//Declarations section
+#define TAM 100
 
 /*
  * 
  */
-int main(int argc, char** argv) {
+int main() {
 
-    return (EXIT_SUCCESS);
+    char ** items;
+    int i, num, background;
+    char expresion[TAM];
+    char command[10];
+    char out_buf[100];
+    pid_t pid;
+    int flag = 0;
+
+    
+    do {
+        printf("%s@my-cli:%s$ ", getenv("USER"),getenv("PWD"));
+        fgets(expresion, TAM, stdin);
+        num = separaItems(expresion, &items, &background);
+        if(strcmp(items[0],"mypwd")==0){
+            pid = fork();
+            
+            if (pid == 0) {
+                printf( "%s%s\n","Your CWD is: ",getcwd( out_buf, -1 ) );
+            }else{
+                wait(NULL);
+            } 
+        }else if (strcmp(items[0],"myexit")==0) {
+            processCommand(&flag, &command);
+        }
+        
+    }while (flag != -1);
+    
+}
+
+void processCommand(int* flag, char* command){
+    
+    printf( "%s","my-cli will exit. Do you want to continue.? y/n ");
+    fgets(command, 10, stdin);
+    if (strcmp(command,"y") == 0 || strcmp(command,"Y") == 0) {
+        flag = -1;
+    }
+
 }
 
